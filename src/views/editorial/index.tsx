07dabedc72d59aee4eb69c9491378ead809fd82d
david3vc <dveracabanillas@hotmail.com>
2022-12-30 14:34:03 -0500

    Reat Table Editorial

--- src/views/editorial/index.tsx
+++ src/views/editorial/index.tsx
@@ -3,6 +3,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import {
+	createColumnHelper,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from '@tanstack/react-table';
 import { EditorialService } from '../../services';
 import { EditorialModel, RequestPagination, ResponsePagination } from '../../types';
 
@@ -16,6 +23,33 @@ const index = (): JSX.Element => {
 		void findAllEditorial();
 		void paginatedSearch();
 	}, []);
+
+	// Vendor
+	const columnHelper = createColumnHelper<EditorialModel>();
+
+	const columns = [
+		columnHelper.accessor('id', {
+			cell: info => info.getValue(),
+		}),
+		columnHelper.accessor('codigo', {
+			cell: info => info.getValue(),
+		}),
+		columnHelper.accessor('nombre', {
+			cell: info => info.getValue(),
+		}),
+		columnHelper.accessor('fechaRegistro', {
+			cell: info => info.getValue(),
+		}),
+		columnHelper.accessor('estado', {
+			cell: info => info.getValue(),
+		}),
+	];
+
+	const table = useReactTable({
+		data: dataEditorial?.data ?? [],
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
 
 	// Methods
 	const findAllEditorial = async (): Promise<void> => {
@@ -63,7 +97,38 @@ const index = (): JSX.Element => {
 				<Col xs={12}>
 					<Card>
 						<Card.Header>Listado de editoriales</Card.Header>
-						<Card.Body></Card.Body>
+						<Card.Body>
+							<Table>
+								<thead>
+									{table.getHeaderGroups().map(headerGroup => (
+										<tr key={headerGroup.id}>
+											{headerGroup.headers.map(header => (
+												<th key={header.id}>
+													{header.isPlaceholder
+														? null
+														: flexRender(header.column.columnDef.header, header.getContext())}
+												</th>
+											))}
+										</tr>
+									))}
+								</thead>
+								<tbody>
+									{
+										table.getRowModel().rows.map(row => (
+											<tr key={row.id}>
+												{
+													row.getVisibleCells().map(cell => (
+														<td key={cell.id}>
+															{flexRender(cell.column.columnDef.cell, cell.getContext())}
+														</td>
+													))
+												}
+											</tr>
+										))
+									}
+								</tbody>
+							</Table>
+						</Card.Body>
 					</Card>
 				</Col>
 			</Row>
